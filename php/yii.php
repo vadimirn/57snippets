636d18411ab6d1396395cbd36c82290825f8d298
Andrey Korchak <57uff3r@gmail.com>
2012-11-02 18:13:18 +0700

    Netbeans hotkeys and Yii snippets

--- php/yii.php
+++ php/yii.php
@@ -13,3 +13,15 @@
 
 Yii::app()->params['param_name'];
 
+/**
+ * Првильный LIKE поиск
+ */
+
+$this->name = addcslashes($this->name, '%_'); //Убираем символы, чтобы не сломать LIKE
+
+$q = new CDbCriteria( array(
+    'condition' => "country_id = :country_id AND name LIKE :name", // Никаких кавычек вокруг :name
+    'params'    => array(':name' => "%$this->name", ':country_id' => $this->country_id)
+) );
+
+return City::model()->findAll($q);

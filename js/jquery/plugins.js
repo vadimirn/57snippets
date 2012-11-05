0717476b4d150ba08afc68627cb9705cf4ab56a4
Andrey Korchak <57uff3r@gmail.com>
2012-11-05 12:48:38 +0700

    Пара git комманд, установка софта в Ubuntu после установки OS.

--- js/jquery/plugins.js
+++ js/jquery/plugins.js
@@ -5,16 +5,16 @@
  */
 
 $('.chzn-choices input').autocomplete({
-source: function( request, response ) {
-  $.ajax({
-      url: "/change/name/autocomplete/"+request.term+"/",
-      dataType: "json",
-      beforeSend: function(){$('ul.chzn-results').empty();},
-      success: function( data ) {
-         response( $.map( data, function( item ) {
-            $('ul.chzn-results').append('<li class="active-result">' + item.name +       '</li>');
-
-      }
-   });
-}
+    source : function( request, response ) {
+      $.ajax({
+          url: "/change/name/autocomplete/"+request.term+"/",
+          dataType: "json",
+          beforeSend: function(){$('ul.chzn-results').empty();},
+          success: function( data ) {
+             response( $.map( data, function( item ) {
+                $('ul.chzn-results').append('<li class="active-result">' + item.name + '</li>');
+             }));
+          }
+      });
+    }
 });

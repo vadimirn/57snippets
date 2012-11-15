f9ece4ca20c8af6466f35281e2b2f5dba1020255
Andrey Korchak <57uff3r@gmail.com>
2012-11-15 14:14:20 +0700

    Паджинация в Yii

--- php/yii.php
+++ php/yii.php
@@ -13,3 +13,29 @@
 
 Yii::app()->params['param_name'];
 
+
+/**
+ * Паджинация в YII
+ *
+ */
+
+// в контроллере
+$criteria           = new CDbCriteria();
+$count              = News::model()->count($criteria);
+$pages              = new CPagination($count);
+
+$pages->pageSize    = 10;
+$pages->applyLimit($criteria);
+$model              = News::model()->recent()->findAll($criteria);
+
+$this->render('view', array(
+     'model' => $model,
+     'pages' => $pages
+));
+
+// в отображении
+$this->widget('CLinkPager', array(
+    'pages' => $pages,
+));
+
+

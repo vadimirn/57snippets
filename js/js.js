091f3b455957b4467f4b43012a49abeb591f34ea
Andrey Korchak <57uff3r@gmail.com>
2012-12-07 14:07:25 +0700

    Пара JS и html сниппетов

--- js/js.js
+++ js/js.js
@@ -5,4 +5,20 @@
 
 
 // Получить текущий год
-new Date().getFullYear()
+new Date().getFullYear();
+
+// хеш страницы
+window.location.hash.substring(1);
+
+// Добавить перемешивание к массиву
+Array.prototype.shuffle = function() {
+   var i = this.length;
+   while (--i) {
+      var j = Math.floor(Math.random() * (i + 1))
+      var temp = this[i];
+      this[i] = this[j];
+      this[j] = temp;
+   }
+
+   return this;
+};
